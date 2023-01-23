8d7fb16b7c7ba49ae800ea9e4e2d8de12f0ec364
Dre <dredawkins11@gmail.com>
2023-01-23 15:40:23 -0600

    created Engineer class

---
+++ src/Engineer.js
@@ -0,0 +1,13 @@
+import Employee from "./Employee";
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email)
+        this.github = github;
+        this.role = "Engineer"
+    }
+
+    getGithub() {
+        return this.github
+    }
+}
